27f69e061ca888032510fd736ec10541555fe34f
ahmedaziz-code <ahmedaziz.elj@esprit.tn>
2022-10-31 20:58:29 +0000

    change apiUrl

--- src/app/shared/Service/Reglement.service.ts
+++ src/app/shared/Service/Reglement.service.ts
@@ -6,7 +6,7 @@ import {HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class ReglementService {
-  readonly API_URL = 'http://localhost:8089/SpringMVC/reglement';
+  readonly API_URL = 'http://app_achat_back:8089/SpringMVC/reglement';
 
   constructor(private httpClient: HttpClient) {
   }
--- src/app/shared/Service/Stock.service.ts
+++ src/app/shared/Service/Stock.service.ts
@@ -7,7 +7,7 @@ import { HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class StockService {
-  readonly API_URL = 'http://localhost:8089/SpringMVC/stock';
+  readonly API_URL = 'http://app_achat_back:8089/SpringMVC/stock';
 
   constructor(private httpClient: HttpClient) { }
 
