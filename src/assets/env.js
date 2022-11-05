443ce1c63436daee3ef0577540e56218ba479c0b
ahmedaziz-code <ahmedaziz.elj@esprit.tn>
2022-11-05 13:34:06 +0100

    delete extra files

--- src/assets/env.js
+++
@@ -1,7 +0,0 @@
-(function(window) {
-    window["env"] = window["env"] || {};
-  
-    // Environment variables
-    window["env"]["app_achat_back"] = "https://api.myapp.com";
-    window["env"]["debug"] = true;
-  })(this);
