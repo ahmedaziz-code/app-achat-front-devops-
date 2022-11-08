b696714be7bd768c8991955c34c1b43eaab2ed1a
lotfi2020-2021 <lotfi.hmida@esprit.tn>
2022-11-08 11:26:33 +0100

    modification adresse ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.219.251:8089/SpringMVC/'
+  apiUrl: 'http://192.168.1.18:8089/SpringMVC/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.219.251:8089/SpringMVC/'
+  apiUrl: 'http://192.168.1.18:8089/SpringMVC/'
 }
 
 /*
